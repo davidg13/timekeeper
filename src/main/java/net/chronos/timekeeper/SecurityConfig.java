71a55b6f61288f3dca485964a1b5f4a103f7e6ed
davidg13 <davegagne01@gmail.com>
2016-11-02 21:12:45 -0400

    Turn on csrf protection.  Bump spring boot to 1.4.1

--- src/main/java/net/chronos/timekeeper/SecurityConfig.java
+++ src/main/java/net/chronos/timekeeper/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
@@ -13,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //TODO: enable csrf and include token in request headers
-        http    .csrf().disable()
+        http    .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
